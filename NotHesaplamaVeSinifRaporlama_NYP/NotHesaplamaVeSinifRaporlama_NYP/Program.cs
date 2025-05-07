da4f67394d293af76914a0c2f83cddd831ccaabb
Süleyman Kaplan <234410107@ogr.kastamonu.edu.tr>
2025-05-07 18:14:45 +0300

    first commit

--- NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/Program.cs
+++ NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/Program.cs
@@ -16,7 +16,7 @@ namespace NotHesaplamaVeSinifRaporlama_NYP
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new login());
         }
     }
 }
