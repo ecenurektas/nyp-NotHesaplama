07f855eb91357502f1a05b8b1c4cb89b8151e160
Süleyman Kaplan <234410107@ogr.kastamonu.edu.tr>
2025-05-08 20:33:30 +0300

    v

--- NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/ogretmen.Designer.cs
+++ NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/ogretmen.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğretmen İsmi:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Öğretmen Soyismi:";
+            // 
+            // ogretmen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1060, 698);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "ogretmen";
             this.Text = "ogretmen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
