a850805cf8f5876d0642bd52ad46929ba553bd83
suleymankaplan <234410107@ogr.kastamonu.edu.tr>
2025-06-02 16:19:07 +0300

    öğrenci deneme

--- NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/ogrenci.Designer.cs
+++ NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/ogrenci.Designer.cs
@@ -39,6 +39,7 @@
             this.ynoTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
             this.OgrenciPanel = new System.Windows.Forms.Panel();
             this.materialButton1 = new MaterialSkin.Controls.MaterialButton();
+            this.materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.OgrenciPanel.SuspendLayout();
             this.SuspendLayout();
@@ -48,14 +49,15 @@
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.EnableHeadersVisualStyles = false;
-            this.dataGridView1.Location = new System.Drawing.Point(143, 313);
+            this.dataGridView1.Location = new System.Drawing.Point(214, 489);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(932, 385);
+            this.dataGridView1.Size = new System.Drawing.Size(1398, 602);
             this.dataGridView1.TabIndex = 12;
             // 
             // materialLabel1
@@ -63,10 +65,11 @@
             this.materialLabel1.Depth = 0;
             this.materialLabel1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.materialLabel1.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel1.Location = new System.Drawing.Point(140, 144);
+            this.materialLabel1.Location = new System.Drawing.Point(210, 225);
+            this.materialLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(132, 33);
+            this.materialLabel1.Size = new System.Drawing.Size(198, 52);
             this.materialLabel1.TabIndex = 14;
             this.materialLabel1.Text = "Öğrenci Numarası:";
             // 
@@ -77,12 +80,14 @@
             this.noTextBox1.Depth = 0;
             this.noTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.noTextBox1.LeadingIcon = null;
-            this.noTextBox1.Location = new System.Drawing.Point(344, 141);
+            this.noTextBox1.Location = new System.Drawing.Point(516, 220);
+            this.noTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.noTextBox1.MaxLength = 50;
             this.noTextBox1.MouseState = MaterialSkin.MouseState.OUT;
             this.noTextBox1.Multiline = false;
             this.noTextBox1.Name = "noTextBox1";
-            this.noTextBox1.Size = new System.Drawing.Size(208, 36);
+            this.noTextBox1.ReadOnly = true;
+            this.noTextBox1.Size = new System.Drawing.Size(312, 36);
             this.noTextBox1.TabIndex = 15;
             this.noTextBox1.Text = "";
             this.noTextBox1.TrailingIcon = null;
@@ -94,10 +99,11 @@
             this.materialLabel2.Depth = 0;
             this.materialLabel2.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.materialLabel2.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel2.Location = new System.Drawing.Point(192, 233);
+            this.materialLabel2.Location = new System.Drawing.Point(288, 364);
+            this.materialLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
-            this.materialLabel2.Size = new System.Drawing.Size(80, 23);
+            this.materialLabel2.Size = new System.Drawing.Size(120, 36);
             this.materialLabel2.TabIndex = 16;
             this.materialLabel2.Text = "Danışman:";
             // 
@@ -108,12 +114,14 @@
             this.danismanTextBox1.Depth = 0;
             this.danismanTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.danismanTextBox1.LeadingIcon = null;
-            this.danismanTextBox1.Location = new System.Drawing.Point(344, 231);
+            this.danismanTextBox1.Location = new System.Drawing.Point(516, 361);
+            this.danismanTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.danismanTextBox1.MaxLength = 50;
             this.danismanTextBox1.MouseState = MaterialSkin.MouseState.OUT;
             this.danismanTextBox1.Multiline = false;
             this.danismanTextBox1.Name = "danismanTextBox1";
-            this.danismanTextBox1.Size = new System.Drawing.Size(208, 36);
+            this.danismanTextBox1.ReadOnly = true;
+            this.danismanTextBox1.Size = new System.Drawing.Size(312, 36);
             this.danismanTextBox1.TabIndex = 17;
             this.danismanTextBox1.Text = "";
             this.danismanTextBox1.TrailingIcon = null;
@@ -126,7 +134,8 @@
             this.materialLabel3.Depth = 0;
             this.materialLabel3.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.materialLabel3.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel3.Location = new System.Drawing.Point(653, 144);
+            this.materialLabel3.Location = new System.Drawing.Point(980, 225);
+            this.materialLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
             this.materialLabel3.Size = new System.Drawing.Size(56, 19);
@@ -140,12 +149,14 @@
             this.donemTextBox1.Depth = 0;
             this.donemTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.donemTextBox1.LeadingIcon = null;
-            this.donemTextBox1.Location = new System.Drawing.Point(776, 141);
+            this.donemTextBox1.Location = new System.Drawing.Point(1164, 220);
+            this.donemTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.donemTextBox1.MaxLength = 50;
             this.donemTextBox1.MouseState = MaterialSkin.MouseState.OUT;
             this.donemTextBox1.Multiline = false;
             this.donemTextBox1.Name = "donemTextBox1";
-            this.donemTextBox1.Size = new System.Drawing.Size(208, 36);
+            this.donemTextBox1.ReadOnly = true;
+            this.donemTextBox1.Size = new System.Drawing.Size(312, 36);
             this.donemTextBox1.TabIndex = 19;
             this.donemTextBox1.Text = "";
             this.donemTextBox1.TrailingIcon = null;
@@ -157,10 +168,11 @@
             this.materialLabel4.Depth = 0;
             this.materialLabel4.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.materialLabel4.Font = new System.Drawing.Font("Roboto", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-            this.materialLabel4.Location = new System.Drawing.Point(672, 233);
+            this.materialLabel4.Location = new System.Drawing.Point(1008, 364);
+            this.materialLabel4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel4.Name = "materialLabel4";
-            this.materialLabel4.Size = new System.Drawing.Size(37, 19);
+            this.materialLabel4.Size = new System.Drawing.Size(56, 30);
             this.materialLabel4.TabIndex = 21;
             this.materialLabel4.Text = "YNO:";
             // 
@@ -171,12 +183,14 @@
             this.ynoTextBox1.Depth = 0;
             this.ynoTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.ynoTextBox1.LeadingIcon = null;
-            this.ynoTextBox1.Location = new System.Drawing.Point(776, 231);
+            this.ynoTextBox1.Location = new System.Drawing.Point(1164, 361);
+            this.ynoTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.ynoTextBox1.MaxLength = 50;
             this.ynoTextBox1.MouseState = MaterialSkin.MouseState.OUT;
             this.ynoTextBox1.Multiline = false;
             this.ynoTextBox1.Name = "ynoTextBox1";
-            this.ynoTextBox1.Size = new System.Drawing.Size(208, 36);
+            this.ynoTextBox1.ReadOnly = true;
+            this.ynoTextBox1.Size = new System.Drawing.Size(312, 36);
             this.ynoTextBox1.TabIndex = 22;
             this.ynoTextBox1.Text = "";
             this.ynoTextBox1.TrailingIcon = null;
@@ -185,6 +199,7 @@
             // 
             // OgrenciPanel
             // 
+            this.OgrenciPanel.Controls.Add(this.materialTextBox1);
             this.OgrenciPanel.Controls.Add(this.ynoTextBox1);
             this.OgrenciPanel.Controls.Add(this.materialLabel4);
             this.OgrenciPanel.Controls.Add(this.donemTextBox1);
@@ -194,9 +209,10 @@
             this.OgrenciPanel.Controls.Add(this.noTextBox1);
             this.OgrenciPanel.Controls.Add(this.materialLabel1);
             this.OgrenciPanel.Controls.Add(this.dataGridView1);
-            this.OgrenciPanel.Location = new System.Drawing.Point(168, 101);
+            this.OgrenciPanel.Location = new System.Drawing.Point(252, 158);
+            this.OgrenciPanel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.OgrenciPanel.Name = "OgrenciPanel";
-            this.OgrenciPanel.Size = new System.Drawing.Size(1154, 769);
+            this.OgrenciPanel.Size = new System.Drawing.Size(1731, 1202);
             this.OgrenciPanel.TabIndex = 23;
             // 
             // materialButton1
@@ -207,12 +223,12 @@
             this.materialButton1.Depth = 0;
             this.materialButton1.HighEmphasis = true;
             this.materialButton1.Icon = null;
-            this.materialButton1.Location = new System.Drawing.Point(17, 93);
-            this.materialButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.materialButton1.Location = new System.Drawing.Point(26, 145);
+            this.materialButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.materialButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialButton1.Name = "materialButton1";
             this.materialButton1.NoAccentTextColor = System.Drawing.Color.Empty;
-            this.materialButton1.Size = new System.Drawing.Size(94, 38);
+            this.materialButton1.Size = new System.Drawing.Size(141, 59);
             this.materialButton1.TabIndex = 13;
             this.materialButton1.Text = "Çıkış Yap";
             this.materialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
@@ -220,16 +236,35 @@
             this.materialButton1.UseVisualStyleBackColor = true;
             this.materialButton1.Click += new System.EventHandler(this.materialButton1_Click);
             // 
+            // materialTextBox1
+            // 
+            this.materialTextBox1.AnimateReadOnly = false;
+            this.materialTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialTextBox1.Depth = 0;
+            this.materialTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBox1.LeadingIcon = null;
+            this.materialTextBox1.Location = new System.Drawing.Point(597, 93);
+            this.materialTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.materialTextBox1.MaxLength = 50;
+            this.materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialTextBox1.Multiline = false;
+            this.materialTextBox1.Name = "materialTextBox1";
+            this.materialTextBox1.ReadOnly = true;
+            this.materialTextBox1.Size = new System.Drawing.Size(312, 36);
+            this.materialTextBox1.TabIndex = 23;
+            this.materialTextBox1.Text = "";
+            this.materialTextBox1.TrailingIcon = null;
+            this.materialTextBox1.UseAccent = false;
+            this.materialTextBox1.UseTallSize = false;
+            // 
             // ogrenci
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1481, 903);
+            this.ClientSize = new System.Drawing.Size(2222, 1411);
             this.Controls.Add(this.OgrenciPanel);
             this.Controls.Add(this.materialButton1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "ogrenci";
-            this.Padding = new System.Windows.Forms.Padding(2, 41, 2, 2);
             this.Text = "Öğrenci";
             this.Load += new System.EventHandler(this.ogrenci_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -251,5 +286,6 @@
         private MaterialSkin.Controls.MaterialTextBox ynoTextBox1;
         private System.Windows.Forms.Panel OgrenciPanel;
         private MaterialSkin.Controls.MaterialButton materialButton1;
+        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
     }
 }
--- NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/ogrenci.cs
+++ NotHesaplamaVeSinifRaporlama_NYP/NotHesaplamaVeSinifRaporlama_NYP/ogrenci.cs
@@ -11,6 +11,7 @@ using System.Data.SqlClient;
 using OrnekProje;
 using MaterialSkin;
 using MaterialSkin.Controls;
+using System.Diagnostics;
 
 
 namespace NotHesaplamaVeSinifRaporlama_NYP
@@ -23,12 +24,59 @@ namespace NotHesaplamaVeSinifRaporlama_NYP
             DesignManager.ApplyTheme(this);
             DesignManager.CenterControl(this, OgrenciPanel);
         }
+        private void ortalamaHesapla()
+        {
+            Database.InitializeConnection();
+            double toplamNot = 0.0;
+            int gecerliNotSayisi = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (!row.IsNewRow)
+                {
+                    object harfNotuObj = row.Cells["HarfNotu"].Value;
+                    if (harfNotuObj == DBNull.Value || string.IsNullOrWhiteSpace(harfNotuObj.ToString()))
+                    {
+                        continue;
+                    }
+                    materialTextBox1.Text+=gecerliNotSayisi.ToString() + " - " + harfNotuObj.ToString() + "\n";
+                    string harfNotu = harfNotuObj.ToString();
+                    double notDegeri = 0.0;
+
+                    switch (harfNotu)
+                    {
+                        case "AA": notDegeri = 4.0; break;
+                        case "BA": notDegeri = 3.5; break;
+                        case "BB": notDegeri = 3.0; break;
+                        case "CB": notDegeri = 2.5; break;
+                        case "CC": notDegeri = 2.0; break;
+                        case "DC": notDegeri = 1.5; break;
+                        case "DD": notDegeri = 1.0; break;
+                        case "FF": notDegeri = 0.0; break;
+                        default: continue;
+                    }
+
+                    toplamNot += notDegeri;
+                    gecerliNotSayisi++;
+                }
+            }
+
+            if (gecerliNotSayisi > 0)
+            {
+                double ortalama = toplamNot / gecerliNotSayisi;
+                ynoTextBox1.Text = ortalama.ToString();
+            }
+            else
+            {
+                ynoTextBox1.Text = "31.00";
+            }
+        }
         private void ogrenci_Load(object sender, EventArgs e)
         {
             DesignManager.StyleDataGridView(dataGridView1);
             string ogrenciID = login.loginUserID.ToString();
             string OgretimGrID = "";
-
+            ortalamaHesapla();
             try
             {
                 using (SqlCommand command = new SqlCommand("SELECT * FROM Ogrenci WHERE OgrenciID = @ogrenciID", Database.Connection))
@@ -40,7 +88,7 @@ namespace NotHesaplamaVeSinifRaporlama_NYP
                     {
                         noTextBox1.Text = reader["OgrenciID"].ToString();
                         donemTextBox1.Text = reader["OgrenciDonem"].ToString();
-                        ynoTextBox1.Text = reader["YNO"].ToString();
+                        //ynoTextBox1.Text = reader["YNO"].ToString();
                         OgretimGrID = reader["OgretimGrID"].ToString();
                     }
                     reader.Close();
